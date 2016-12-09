41055428ca182b593701a3c759713b0bbbbff636
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-12-09 23:56:39 +0000

    Add core structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// Set up your application entry point here...
+import React from 'react';
+import { render } from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+import '../node_modules/bootstrap/dist/css//bootstrap.css';
+
+render(
+  <Router history={browserHistory} routes={routes}/>,
+  document.getElementById('app')
+);
