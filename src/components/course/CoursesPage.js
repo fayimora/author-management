32da4ea7ddc0e70ba9c64a58bd22c8b6f8aaf4f9
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-12-10 02:48:55 +0000

    Add initial form for creating courses

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      course: {title: ""}
+    };
+    this.onTitleChanged = this.onTitleChanged.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChanged(e) {
+    const course = this.state.course;
+    course.title = e.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSave() {
+    alert(`Saved ${this.state.course.title}`);
+  }
+
   render () {
     return (
       <div>
-        <hi>Courses</hi>
+        <h1>Courses</h1>
+
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChanged}
+          value={this.state.course.title}/>
+
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave}/>
       </div>
     );
   }
