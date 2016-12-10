ca73d3b03b2732b0a9fa03b7b213b79e06bd19fe
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-12-10 00:02:01 +0000

    Add courses page

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CoursesPage extends React.Component {
+  render () {
+    return (
+      <div>
+        <hi>Courses</hi>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
