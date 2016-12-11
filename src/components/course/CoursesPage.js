17130c949c1d90648a53479ef045a065d9e95010
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-12-11 01:45:31 +0000

    Finish wiring up redux :wink:

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -17,14 +20,18 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    alert(`Saved ${this.state.course.title}`);
+    this.props.actions.createCourse(this.state.course);
+  }
+
+  courseRow(course, idx) {
+    return <div key={idx}>{course.title}</div>;
   }
 
   render () {
     return (
       <div>
         <h1>Courses</h1>
-
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input
           type="text"
@@ -40,4 +47,21 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
